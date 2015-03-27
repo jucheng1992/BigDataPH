7ea83f7b0a1df9a4656730079241066cc0fc1819
Cheng <jucheng1992@gmail.com>
2015-03-26 21:16:28 -0700

    AWS

---
+++ Parallel_R/Winedata.R
@@ -0,0 +1,28 @@
+library(doParallel)
+library(randomForest)
+
+winedata<-read.csv2("winequality-white.csv", header = TRUE)
+
+head(winedata)
+
+curr<-data.matrix(winedata)
+
+prf.time<-system.time({
+      cl <- makeCluster(4)
+      registerDoParallel(cl)
+      ## each cluster, we set 2500 trees
+      rf <- foreach(ntree=rep(250, 4), 
+                    .combine=combine,
+                    .packages='randomForest') %dopar%
+            randomForest(as.factor(quality)~., data=curr, ntree=ntree)
+      stopCluster(cl)
+})
+
+srf.time<-system.time(
+      randomForest(as.factor(quality)~., curr, ntree=1000)
+)
+
+prf.time
+srf.time
+summary(rf)
+importance(rf)
