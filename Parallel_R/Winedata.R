3801cd0205d6ab2c3cbb76aef6d224e534b60b9c
Cheng <jucheng1992@gmail.com>
2015-03-26 21:40:46 -0700

    AWS

--- Parallel_R/Winedata.R
+++ Parallel_R/Winedata.R
@@ -11,7 +11,7 @@ prf.time<-system.time({
       cl <- makeCluster(4)
       registerDoParallel(cl)
       ## each cluster, we set 2500 trees
-      rf <- foreach(ntree=rep(250, 4), 
+      rf <- foreach(ntree=rep(2500, 4), 
                     .combine=combine,
                     .packages='randomForest') %dopar%
             randomForest(as.factor(quality)~., data=curr, ntree=ntree)
